fd29d065620febdca4fbcf0dc286bc92c1747333
khanh703 <khanh703@gmail.com>
2022-02-20 11:04:28 -0700

    Added predict-unemployed route that handles the new prediction using new random forest model with education attainment.

--- Flask_presentation/app.py
+++ Flask_presentation/app.py
@@ -39,11 +39,61 @@ def analysis():
 
 
 def returnStateOptions():
-    states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+    states = ['Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'NewHampshire',
+    'NewJersey',
+    'NewMexico',
+    'NewYork',
+    'NorthCarolina',
+    'NorthDakota',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'RhodeIsland',
+    'SouthCarolina',
+    'SouthDakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'WestVirginia',
+    'Wisconsin',
+    'Wyoming']
 
     return states
 
 Employment_by_County = appPath + '/Flask_presentation/static/data_files/Employment_by_County.csv'
+Employment_Education_by_County = appPath + '/Flask_presentation/static/data_files/Employment_by_County_with_education_info.csv'
 
 # Todo: link up to CSV/DB
 # Todo: SQL : SELECT DISTINCT county FROM db.table;
@@ -66,9 +116,23 @@ def returnCountyDemographics():
     results = results_.apply(lambda x: x.to_json(), axis=1)
     return (results.to_list()[0])
 
+def returnCountyDemographicsEducation():
+    df = pd.read_csv(Employment_Education_by_County)
+    #df = pd.read_csv('/app/Flask_presentation/static/data_files/Employment_by_County.csv', dtype=str)
+    search_county = request.args.get("county").replace("%20", " ")
+    search_county = unquote(search_county)
+    results_ = df[df['county'].str.contains(search_county)]
+    results = results_.apply(lambda x: x.to_json(), axis=1)
+    return (results.to_list()[0])
+
 @app.route("/getCountyDemogrographic", methods = ['GET', 'POST'])
 def getCountyDemogrographic():
     demographic = returnCountyDemographics()
+    return jsonify({"demographic":demographic})
+
+@app.route("/getCountyDemogrographicEducation", methods = ['GET', 'POST'])
+def getCountyDemogrographicEducation():
+    demographic = returnCountyDemographicsEducation()
     return jsonify({"demographic":demographic})
 
 
@@ -127,11 +191,49 @@ def makePrediction():
             "prediction_unemployed_feature_importance":prediction_unemployment_pct_feature_importance
         })
 
-
 @app.route("/predict", methods = ['GET', 'POST'])
 def predict():
     return render_template('predict.html', 
         states_options = returnStateOptions())
+
+
+
+
+@app.route("/predict-unemployed", methods = ['GET', 'POST'])
+def predictUnemployed():
+    return render_template('predict-unemployed.html', 
+        states_options = returnStateOptions())
+
+
+@app.route("/makeUnemployedPrediction", methods = ['POST'])
+def makeUnemployedPrediction():
+    feature_columns = ['race_white', 'race_black', 'race_asian','race_others','completed_college_county','not_completed_college_county']
+    if request.json:
+
+        # Extra json input
+        race_white = request.json.get('race_white')
+        race_black = request.json.get('race_black')
+        race_asian = request.json.get('race_asian')
+        race_others = request.json.get('race_others')
+        completed_college_county = request.json.get('completed_college_county')
+        not_completed_college_county = request.json.get('not_completed_college_county')
+
+        # Labor force prediction
+        #filename_labor_force = '/app/Flask_presentation/static/model/finalized_model_labor_force.sav'
+        filename_labor_force = appPath + '/Flask_presentation/static/model/finalized_model_variation_education.sav'
+        model_unemployed = pickle.load(open(filename_labor_force, 'rb'))
+        prediction_unemployed = model_unemployed.predict([[race_white,race_black,race_asian,race_others,completed_college_county, not_completed_college_county]])[0]
+        prediction_unemployed_feature_importance_ = sorted(zip(model_unemployed.feature_importances_, feature_columns), reverse=True)
+        prediction_unemployed_feature_importance = json.dumps(prediction_unemployed_feature_importance_)
+
+        
+        # Return data in json format
+        prediction_unemployed = round(prediction_unemployed)
+
+        return jsonify({
+            "prediction_unemployed": prediction_unemployed, 
+            "prediction_unemployed_feature_importance":prediction_unemployed_feature_importance
+        })
 
 if __name__ == '__main__':
     app.run()
